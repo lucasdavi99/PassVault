5d548e39fe6768c5bf6d0b6b4e27c710f36b89b2
lucasdavi99 <lucasdavi99@hotmail.com>
2025-02-06 16:13:38 -0300

    feat: Nova entidade

---
+++ PassVault/Models/Folder.cs
@@ -0,0 +1,18 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassVault.Models
+{
+    class Folder
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Created { get; set; }
+        public List<Account> Accounts { get; set; } = new List<Account>();
+    }
+}
