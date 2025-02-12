a49d01120b77e49cc5ef3f88cf6c2577ecdeac51
lucasdavi99 <lucasdavi99@hotmail.com>
2025-02-11 21:42:29 -0300

    feat: entidade de Backup criada

---
+++ PassVault/Models/BackupData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassVault.Models
+{
+    class BackupData
+    {
+        public DateTime ExportDate { get; set; }
+        public List<Account> Accounts { get; set; }
+        public List<Folder> Folders { get; set; }
+    }
+}
