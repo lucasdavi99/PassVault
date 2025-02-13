35f07e468efcf7c585098e89e50b74aeeaa14c1c
lucasdavi99 <lucasdavi99@hotmail.com>
2025-02-13 17:42:04 -0300

    refactor: limpeza de código

--- PassVault/Views/MainPage.xaml.cs
+++ PassVault/Views/MainPage.xaml.cs
@@ -14,12 +14,10 @@ public partial class MainPage : ContentPage
     {
         base.OnAppearing();
 
-        // Verifica se o alerta de delete já foi exibido
         bool hasSeenDeleteHint = Preferences.Get("HasSeenDeleteHint", false);
         if (!hasSeenDeleteHint)
         {
             await Shell.Current.DisplayAlert("Dica", "Para deletar uma conta ou pasta, toque duas vezes no item.", "OK");
-            // Marca como exibido
             Preferences.Set("HasSeenDeleteHint", true);
         }
     }
