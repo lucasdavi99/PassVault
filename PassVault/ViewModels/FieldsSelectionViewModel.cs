11dba56b82258d75868ecfa525f6bd12cbe6e8bd
lucasdavi99 <lucasdavi99@hotmail.com>
2025-03-08 18:14:02 -0300

    feat: Pagina de seleção de campo criada

---
+++ PassVault/ViewModels/FieldsSelectionViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+
+namespace PassVault.ViewModels
+{
+    public partial class FieldsSelectionViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private bool isUsernameChecked = true;
+
+        [ObservableProperty]
+        private bool isEmailChecked = true;
+
+        [RelayCommand]
+        private async Task ConfirmSelectionAsync()
+        {
+            var selectedFields = new Dictionary<string, bool>
+            {
+                { "Username", IsUsernameChecked },
+                { "Email", IsEmailChecked },               
+            };
+
+            WeakReferenceMessenger.Default.Send(selectedFields);
+            await Shell.Current.Navigation.PopModalAsync();
+        }
+    }
+}
