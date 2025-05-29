5f6dc9985903092e7f21813e0770c7c010712d81
Lucas Davi <lucasdavi99@hotmail.com>
2025-05-29 14:22:43 -0300

    feat: Implementa timeout automático após inatividade de 5 segundos

--- PassVault/App.xaml.cs
+++ PassVault/App.xaml.cs
@@ -1,15 +1,22 @@
-﻿namespace PassVault
+﻿using PassVault.Services;
+
+namespace PassVault
 {
     public partial class App : Application
     {
+        private readonly InactivityTimeoutService _inactivityService;
+
         public App()
         {
             InitializeComponent();
+
+            _inactivityService = new InactivityTimeoutService();
+            _inactivityService.TimeoutElapsed += OnInactivityTimeout;
         }
 
         protected override Window CreateWindow(IActivationState activationState)
         {
-            Window window = new Window(new AppShell());            
+            Window window = new Window(new AppShell());
             return window;
         }
 
@@ -23,6 +30,26 @@
                 await Shell.Current.GoToAsync("//TutorialPage1");
             else
                 await Shell.Current.GoToAsync("//LockScreen");
+        }
+
+        protected override void OnSleep()
+        {
+            base.OnSleep();
+            _inactivityService.Start();
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            _inactivityService.Stop();
+        }
+
+        private async void OnInactivityTimeout()
+        {
+            await MainThread.InvokeOnMainThreadAsync(async () =>
+            {
+                await Shell.Current.GoToAsync("//LockScreen");
+            });
         }
     }
 }
---
+++ PassVault/Services/InactivityTimeoutService.cs
@@ -0,0 +1,27 @@
+﻿using Timer = System.Timers.Timer;
+
+
+namespace PassVault.Services
+{
+    public class InactivityTimeoutService
+    {
+        private readonly Timer _timer;
+        private readonly TimeSpan _timeout = TimeSpan.FromMinutes(1);
+        public event Action TimeoutElapsed = delegate { };
+
+        public InactivityTimeoutService()
+        {
+            _timer = new Timer(_timeout.TotalMilliseconds);
+            _timer.Elapsed += (s, e) => TimeoutElapsed?.Invoke();
+            _timer.AutoReset = false;
+        }
+
+        public void Start() => _timer.Start();
+        public void Stop() => _timer.Stop();
+        public void Reset()
+        {
+            _timer.Stop();
+            _timer.Start();
+        }
+    }
+}
