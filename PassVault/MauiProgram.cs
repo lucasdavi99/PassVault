085d353dcadf34caa7e11904d82a30e1e7536ccd
Lucas Davi <lucasdavi99@hotmail.com>
2025-09-21 16:18:25 -0300

    feat: removendo teste de tutorial

--- PassVault/MauiProgram.cs
+++ PassVault/MauiProgram.cs
@@ -89,7 +89,7 @@ namespace PassVault
 #if DEBUG
             builder.Logging.AddDebug();
             // Força o tutorial a ser exibido em modo de depuração para testes
-            Preferences.Set("IsNewUser", true);
+            //Preferences.Set("IsNewUser", true);
 #endif
 
             // Build da aplicação
